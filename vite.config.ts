0351b91fb74cb2bf65da4615e76312c6cf75cdb2
renchang.peng <renchang.peng@riversecurity.com>
2024-12-05 15:09:48 +0800

    fix: 删除

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { inspectorServer } from "@react-dev-inspector/vite-plugin";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), inspectorServer()],
-});
